7f2353f7c7c9bfe802b5d2628d485496328fa622
Xola Bangaza <xolabangaza@gmail.com>
2024-04-30 15:17:11 +0200

    controller

---
+++ controller/UserController.js
@@ -0,0 +1,38 @@
+const db = require("../models")
+const User = db.User
+
+// Creating  a user
+exports.create = async (req, res)=>{
+    if(!req.body){
+        res.status(400).send("Cannot add without info")
+        return;
+    }
+    let results;
+
+    await fetch('https://')
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(results=data.results[0].email)})
+
+      const user = new User({
+        username: req.body.username,
+        email: results,
+        password: req.body.password,
+        address: req.body.address,
+        contactInfo: req.body.contactInfo
+        
+      })  
+
+      try{
+        user.save()
+        .then(user=>{
+           
+            console.log(user)
+            res.send(user)
+        })
+        return
+      }catch (err){
+                  res.status(500).send('Could not create a new user')
+                  console.log(`Some err occured : ${err.message}`)
+  }
+}
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require ("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+
+
+mongoose.connect("mongodb+srv://xolabangaza:QRUc7lWbDHHkhY4v@cluster0.v2o51wq.mongodb.net/mix_mart?")
+.then(()=>{
+    console.log("Connected to DB succesfully")
+})
 
 
 
